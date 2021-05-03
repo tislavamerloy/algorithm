f8e48010d42fb3db8263802e18bf021582ed8d0b
tislavamerloy <83412868+tislavamerloy@users.noreply.github.com>
2021-05-03 17:52:45 +0300

    Add files via upload

---
+++ buble_sort.cpp
@@ -0,0 +1,17 @@
+﻿void buble_sort(int* mass, int size) {
+	bool flag = false;
+	while (flag == false) {
+		flag = true;
+		for (int i = 0; i < size; i++) {
+			for (int ii = i; ii < size; ii++) {
+				if (mass[i] > mass[ii]) {
+					flag = false;
+					int temp = mass[ii];
+					mass[ii] = mass[i];
+					mass[i] = temp;
+					i = ii;
+				}
+			}
+		}
+	}
+}
