e0e2ed8afe9ccd227b7e927f0b453728d234a8df
tislavamerloy <83412868+tislavamerloy@users.noreply.github.com>
2021-05-03 18:05:23 +0300

    Add files via upload

---
+++ binary_search.cpp
@@ -0,0 +1,16 @@
+﻿int binary_search(int* mass, int size, int target) {
+	int  low = 0;
+	int high = size;
+	int  mid = 0;
+	while (low < high - 1) {
+		mid = (high + low) / 2;
+		if (mass[mid] > target) {
+			high = mid;
+		} else {
+			low = mid;
+		}
+	}
+
+	if (low >= 0 && mass[low] == target) return low;
+	else return -1;
+}
